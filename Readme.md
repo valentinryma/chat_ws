f3b22b6ad6b23f9cad387aa776fb616766036b8c
Valentin Rymaszewski <rymaszewski65@gmail.com>
2024-03-13 19:47:43 -0300

    Commit Inicial

---
+++ Readme.md
@@ -0,0 +1,17 @@
+
+# Aplicación de Chat con WebSocket
+Aplicación sencilla de chat para la implementación de WebSockets
+
+## Tecnología
+* Node JS
+* Express JS
+* Handlebars
+* SweetAlerts
+
+## Funcionamiento
+```bash
+npm install 
+```
+```bash
+npm start
+```    
---
+++ src/routes/views.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const router = Router();
+
+router.get('/', (_, res) => {
+    res.render('index', {
+        title: "Aplicación de Chat",
+        useWS: true,
+        useSweetAlert: true,
+        scripts: [
+            'index.js'
+        ],
+        styles: [
+            'style.css'
+        ]
+
+    })
+});
+
+module.exports = router;
